7ebd818ace7d309fb8878345eea48a5b0d594a0e
stella <stellavir11@gmail.com>
2022-08-23 19:47:52 +0300

    HttpResponseRedirect

--- tasksheet/task/urls.py
+++ tasksheet/task/urls.py
@@ -5,5 +5,7 @@ app_name = 'task'
 urlpatterns = [
     path('', views.index, name='index'),
     path('<int:room_id>/', views.detail, name='detail'),
-    path('<int:room_id>/status/', views.status, name='status'),
+    path('<int:room_id>/rate/', views.rate, name='rate'),
+    path('<int:room_id>/results/', views.results, name='results'),
+
 ]
--- tasksheet/task/views.py
+++ tasksheet/task/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 # Create your views here.
-from django.http import HttpResponse
+from django.http import HttpResponseRedirect
 
 from django.urls import reverse
 
@@ -16,19 +16,22 @@ def detail(req, room_id):
     room = get_object_or_404(Room, pk=room_id)
     return render(req, 'task/detail.html', {'room': room})
     # return HttpResponse('You are looking at room %s.' % room_id)
-def status(req, room_id):
+def rate(req, room_id):
     room = get_object_or_404(Room, pk=room_id)
     try:
         selected_status = room.status_set.get(pk=req.POST['status'])
     except (KeyError, Status.DoesNotExist):
-        return render(req, 'task/detail.html', {'room': room, 'erro_message': 'You need to make a decision.', })
+        return render(req, 'task/detail.html', {'room': room, 'error_message': 'You need to make a decision.', })
     else:
-        selected_status.status += 1
+        selected_status.rates += 1
         selected_status.save()
-        return httpResponseRedirect(reverse('task:status', args=(room.id,)))
+        # after post, redirect
+        return HttpResponseRedirect(reverse('task:results', args=(room.id,)))
         # return HttpResponse(resp % room_id)
 
-
+def results(req, room_id):
+    room = get_object_or_404(Room, pk=room_id)
+    return render(req, 'task/results.html', {'room': room})
 
 
 
